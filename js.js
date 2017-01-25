925a9b7dd1cb29de6f2263312033ebf915551c43
Irina <irina.tsedryk@gmail.com>
2017-01-25 23:25:34 +0300

    Task-6-Left menu from json

---
+++ js.js
@@ -0,0 +1,24 @@
+function myFunction_About()
+{
+document.getElementById("cont").innerHTML="about.html";
+}
+
+function myFunction_Managment()
+{
+document.getElementById("cont").innerHTML="managment.html";
+}
+
+function myFunction_Investor()
+{
+document.getElementById("cont").innerHTML="investor.html";
+}
+
+function myFunction_Distributors()
+{
+document.getElementById("cont").innerHTML="distributors.html";
+}
+
+function myFunction_Contact()
+{
+document.getElementById("cont").innerHTML="Contact.html";
+}
