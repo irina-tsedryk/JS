1f9257b820d45ee75658c7e995e9be65a2da60ae
Mikalai Bury <burymm@gmail.com>
2017-02-14 22:35:16 +0300

    add dynamic menu feature

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function loadJSON(filename) {
+function loadJSON(filename, selector) {
     var xhr = new XMLHttpRequest();
 
     xhr.open('GET', filename);
@@ -9,27 +9,50 @@ function loadJSON(filename) {
         if (xhr.status != 200) {
             console.error(xhr.status + ': ' + xhr.statusText);
         } else {
-            runApp(xhr.responseText);
+            runApp(xhr.responseText, selector);
         }
 
     }
 }
 
-function drawMenuElement(selector, data) {
+function createMenuElement(data) {
+    var li, a;
 
+    /* add code to this function*/
+
+    li = document.createElement("li");
+    a = document.createElement("a");
+    a.href = data.link;
+    a.innerText = data.title;
+    li.appendChild(a);
+
+    return li;
 }
 
 function drawMenu(selector, data) {
+    console.log('drawMenu', selector, data);
+    var container = document.querySelector('#' + selector);
 
+    container.innerHTML = '';
+
+    for (var i = 0; i < data.length; i += 1) {
+        var menuElement = createMenuElement(data[i]);
+
+        container.appendChild(menuElement);
+    }
 }
 
-function runApp(json) {
-    var DYNAMIC_MENU_SELECTOR = 'dynamic-menu';
+function runApp(json, selector) {
     var menuData = JSON.parse(json);
 
-    drawMenu(DYNAMIC_MENU_SELECTOR, menuData);
+    drawMenu(selector, menuData);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    loadJSON('js/data/menu.json');
+    loadJSON('js/data/menu.json', 'dynamic-menu');
+    loadJSON('js/data/menu-main.json', 'nav-menu');
+});
+
+document.addEventListener('click', function (event) {
+   console.log(event.target);
 });
